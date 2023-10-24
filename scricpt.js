70fa304d59a907c48e897814e830ac6bb1598ef3
cajty <fdshjgjgffjhd@gmail.com>
2023-10-24 23:04:10 +0100

    brief-3

---
+++ scricpt.js
@@ -0,0 +1,65 @@
+// menu.addEventListener("click",()=>{
+//     if(document.getElementById("menu-contant").style.display === "none")  document.getElementById("menu-contant").style.display = "block";
+//     else  document.getElementById("menu-contant").style.display = "none"
+//  });
+
+//  var p_2 = document.getElementById("num_2_prodt");
+//  var p_5 = document.getElementById("num_5_prodt");
+//  var p_1 = document.getElementById("num_1_prodt");
+
+//  const mois_1 = document.getElementById("mois_1");
+//  const mois_2 = document.getElementById("mois_2");
+//  const mois_5 = document.getElementById("mois_5");
+//  const plus_1 = document.getElementById("plus_1");
+//  const plus_2 = document.getElementById("plus_2");
+//  const plus_5 = document.getElementById("plus_5");
+//  var demor = document.getElementById("demo");
+//  var pp_1 = 1;
+//  var pp_2 = 1; 
+//  var pp_5 = 1;
+
+//  var total = 20 * pp_1 + 18 * pp_1 + 18 * pp_1; 
+
+//  mois_1.addEventListener("click", plus(pp_1,p_1));
+//  mois_2.addEventListener("click", plus(pp_2,p_2));
+//  mois_5.addEventListener("click", plus(pp_5,p_5));
+//  plus_1.addEventListener("click", mois(pp_1,p_1));
+//  plus_2.addEventListener("click", mois(pp_2,p_2));
+//  plus_5.addEventListener("click", mois(pp_2,p_5));
+//  plus_2.addEventListener("click",()=>{
+//     plus(pp_5,p_5)
+// });
+
+
+//  rev1.addEventListener("click",()=>{
+//      document.getElementById("11").style.display = "none"
+//      p_2 = 0;
+//      demor.innerText=  total + ".000$";
+//  });
+//  function plus (x ,prodt ) {
+//     x++
+//     prodt.innerText= x;
+//     demor.innerText=  Number(total)  + ".10$";
+    
+//  }
+
+//  function mois (x, rodt) {
+//      x--;
+//     rodt.innerText= x;
+//     demor.innerText=    p_1 * pp_1 + p_2 * pp_1 + p_5 * pp_1  + ".00$";
+    
+//  }
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
